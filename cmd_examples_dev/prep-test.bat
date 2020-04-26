d393c2a8f92bddfa06cce07f22d6068a75f6376c
m-lange <martin_lange_@gmx.net>
2020-04-27 01:57:32 +0200

    draw border, fix image rotation

--- cmd_examples_dev/prep-test.bat
+++ cmd_examples_dev/prep-test.bat
@@ -3,8 +3,10 @@
   --debug ^
   prep ^
     --output ../test_data/out/*-prep.png ^
-    --format 15cm/10cm ^
-	--padding 1cm/1cm/1cm/1cm ^
-	--margins 5mm/40mm/20mm/5mm ^
+    --format 10cm/15cm ^
+	--padding 5mm/5mm/1cm/5mm ^
+	--margins 5mm ^
+	--border 2px/2px/5px/2px ^
+	--border-color red ^
 	--dpi 300
 pause
